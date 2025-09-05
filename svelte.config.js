d2bd893158f08eb7a30419fa6ffd9124a88d17fa
paoloricciuti <ricciutipaolo@gmail.com>
2025-09-05 20:43:24 +0200

    fix: trust localhost

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,10 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+	kit: { adapter: adapter(), csrf: { trustedOrigins: ['http://localhost:5173'] } }
 };
 
 export default config;
