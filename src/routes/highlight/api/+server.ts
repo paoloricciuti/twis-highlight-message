bcdf67842f25ccf500d6749f61e7df3cad7ea054
paoloricciuti <ricciutipaolo@gmail.com>
2025-09-06 14:14:37 +0200

    chore: log stuff

--- src/routes/highlight/api/+server.ts
+++ src/routes/highlight/api/+server.ts
@@ -4,6 +4,7 @@ import { json } from '@sveltejs/kit';
 export async function POST({ request }) {
 	const user_cookie = request.headers.get('Authorization')?.replace('Bearer ', '');
 	if (!user_cookie || user_cookie !== valid) {
+		console.log({ user_cookie, valid });
 		return json({ message: 'Unauthorized' }, { status: 401 });
 	}
 	const form_data = await request.json();
